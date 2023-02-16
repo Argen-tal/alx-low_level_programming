f94a81727934d023e8ffb87767d4d201b8e68062
Aramide <carlpeacebringer@gmail.com>
2023-02-16 15:17:22 -0800

    Enjoyng C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Output Program
+ */
+{
+	int main(void)
+	    puts("programming is like building a multilingual puzzle\n");
+	    return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof
+}
