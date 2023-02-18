de34c0d88688e4031277b769090fdb95c3e04112
Aramide <carlpeacebringer@gmail.com>
2023-02-18 12:48:21 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The program writes letters a to z without e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z';)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+		putchar(ch);
+		}
+	}
+		putchar('\n');
+
+	return (0);
+}
+
