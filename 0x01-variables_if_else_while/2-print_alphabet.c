eb4b93ff65c79e23373036f2bc0aae6884977a3a
Aramide <carlpeacebringer@gmail.com>
2023-02-18 08:05:07 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The program writes letters a to z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                putchar(ch);
+        }
+
+        return (0);
+}
+
