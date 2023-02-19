3a78c33da5b0162b786bc3946731fd3811a10bec
Aramide <carlpeacebringer@gmail.com>
2023-02-19 07:22:29 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: The program writes one to nine
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
