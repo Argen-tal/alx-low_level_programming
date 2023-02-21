efb297383bdff395572772350bc3a36db67d0652
Aramide <carlpeacebringer@gmail.com>
2023-02-21 07:44:00 -0800

    nesting

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Writes _putchar
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+void main(void)
+{
+	_putchar("_putchar");
+	return (0);
+}
