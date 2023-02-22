190f50e29226d2101781b5a5c72253f2290afc56
Aramide <carlpeacebringer@gmail.com>
2023-02-22 04:41:36 -0800

    Header

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,17 +11,20 @@
 #include <stdio.h>
 
 void print_to_98(int n) {
-    int i;
+int i;
 
-    if (n <= 98) {
-        for (i = n; i < 98; i++) {
-            printf("%d, ", i);
-        }
-    } else {
-        for (i = n; i > 98; i--) {
-            printf("%d, ", i);
-        }
-    }
-    printf("98\n");
+if (n <= 98) {
+for (i = n; i < 98; i++)
+{
+printf("%d, ", i);
+}
+}
+else
+{
+for (i = n; i > 98; i--) {
+printf("%d, ", i);
+}
+}
+printf("98\n");
 }
 
