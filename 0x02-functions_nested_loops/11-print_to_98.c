75f06e28d5b4c8cf750e575d989d30a39afd3c5c
Aramide <carlpeacebringer@gmail.com>
2023-02-22 03:45:09 -0800

    debugging

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,23 +10,18 @@
 
 #include <stdio.h>
 
-void print_to_98(int n)
-{
+void print_to_98(int n) {
+    int i;
+
     if (n <= 98) {
-        for (int i = n; i <= 98; i++) {
-            printf("%d", i);
-            if (i < 98) {
-                printf(", ");
-            }
+        for (i = n; i < 98; i++) {
+            printf("%d, ", i);
         }
     } else {
-        for (int i = n; i >= 98; i--) {
-            printf("%d", i);
-            if (i > 98) {
-                printf(", ");
-            }
+        for (i = n; i > 98; i--) {
+            printf("%d, ", i);
         }
     }
-    printf("\n");
+    printf("98\n");
 }
 
