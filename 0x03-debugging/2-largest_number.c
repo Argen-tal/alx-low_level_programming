90477deb07886cd2c757cb2322c3a32336d1b3cd
Aramide <carlpeacebringer@gmail.com>
2023-02-23 05:08:57 -0800

    fix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,18 +10,16 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = a;
+int largest = a;
 
-	if (b > largest)
-	{
-		largest = b;
-	}
-	if ( c > largest)
-	{
-		largest = c;
-	}
-
-	return (largest);
-
+if (b > largest)
+{
+largest = b;
+}
+if ( c > largest)
+{
+largest = c;
 }
 
+return (largest);
+}
