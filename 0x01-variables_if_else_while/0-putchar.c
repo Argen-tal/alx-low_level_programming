f24ecfb77d07881a5ee5911ad737966435b74314
Aramide <carlpeacebringer@gmail.com>
2023-02-23 04:49:35 -0800

    total

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: The program writes _putchar.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int pt;
+
+	pt = _putchar;
+	_putchar("_putchar");
+
+	return (0)
+}
