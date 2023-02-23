f295ba557b17d9de509f1f9dee899aa4903a51df
Aramide <carlpeacebringer@gmail.com>
2023-02-22 21:58:16 -0800

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entery point
+ * Description: Assign a random number to the variable n                                             * and print whether it is positive, negative, or zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        if (n == 0)
+                printf("0 is zero\n");                    else if (n > 0)
+                printf("%d is positive\n", n);            else
+                printf("%d is negative\n", n);
+
+        return (0);
+}
