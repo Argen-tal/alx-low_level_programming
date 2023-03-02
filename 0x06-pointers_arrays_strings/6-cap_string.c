018391ac43f4095d7a652532599b9e44085465af
Aramide <carlpeacebringer@gmail.com>
2023-03-01 18:23:10 -0800

    quick fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,13 @@
 
 char *cap_string(char *str)
 {
-
 int index = 0;
 
 while (str[index])
 {
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
+
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
@@ -35,3 +35,4 @@ index++;
 }
 return (str);
 }
+
