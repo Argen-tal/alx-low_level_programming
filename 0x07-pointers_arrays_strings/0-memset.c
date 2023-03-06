9f53efe0d05b468fd74a2fa8b7b86ba72033314d
Aramide <carlpeacebringer@gmail.com>
2023-03-06 08:54:53 -0800

    Mem

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+/**
+ * _memset() - Entry point
+ * The functions fills memory with a constant byte
+ *
+ *
+ */
+
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	*_memset(char *s, char b, unsigned int n);
+	return (0);	
+}
