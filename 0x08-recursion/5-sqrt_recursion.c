40c256b8baa3d636c2eb897df4480ec3e9dc56df
Aramide <carlpeacebringer@gmail.com>
2023-03-08 11:45:11 -0800

    mh

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,5 +11,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	return (-1);
-	return (actual_sqrt_recursion(n, 0));
+	return (sqrt_recursion(n, 0));
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int actual_prime(int n, int i)
+int is_prime_number(int n, int i)
 
 /**
  * is_prime_number - says if an integer is a prime number or not
@@ -13,7 +13,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime(n, n - 1));
+	return (is_prime_number(n, n - 1));
 }
 
 
@@ -25,7 +25,7 @@ int is_prime_number(int n)
  * Return: 1 if n is prime, 0 if not
  */
 
-int actual_prime(int n, int i)
+int is_prime_number(int n, int i)
 {
 	if (i == 1)
 		return (1);
