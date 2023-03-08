112829836058df3667576321d977bf1f63c9a3e8
Aramide <carlpeacebringer@gmail.com>
2023-03-08 04:13:01 -0800

    Recur

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+/**
+ * _puts_recursion - Entry point
+ * Description: The function prints a string pointed by *s
+ * @s: pointer
+ * Return: s
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar("%c", *s);
+	_puts_recursion(++s);
+}
