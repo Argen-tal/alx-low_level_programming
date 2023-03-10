103e96d0d4196e71620249ae14289e8ed182f73a
Aramide <carlpeacebringer@gmail.com>
2023-03-10 07:02:33 -0800

    check

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Get a list of all .c files in the current directory
 SRC_FILES=$(ls *.c)
-
-# Compile all .c files into .o files
 for FILE in $SRC_FILES
 do
   gcc -c $FILE
 done
-
-# Create the static library from the .o files
 ar rcs liball.a *.o
-
-# Clean up the .o files
 rm *.o
 
