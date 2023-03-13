aa5ae632c7c1ca6c2495cc964dd2087792bcd502
Aramide <carlpeacebringer@gmail.com>
2023-03-12 23:48:51 -0700

    Fixed0

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - print the number of arguments passed to the program
+ * main - Prints the number of arguments passed to the it
  * @argc: number of arguments
+ * @argv: An array of pointers to the argument
  * Return: Always 0 (Success)
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 printf("%d\n", argc - 1);
+
 return (0);
 }
 
