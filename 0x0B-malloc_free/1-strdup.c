51974bccd41ad02eb0a6d9a36d81d50000a3b12f
Aramide <carlpeacebringer@gmail.com>
2023-03-13 09:19:26 -0700

    Yeh

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strdup - Entry point
  * Description: The function duplicates string
  * @str: string to be used
- * Return: *sru, NULL if sru does not contain value
+ * Return: aaa, NULL if aaa does not contain value
  */
 char *_strdup(char *str)
 {
+char *aaa;
 int i, r = 0;
-char sru;
 
 if (str == NULL)
 return (NULL);
@@ -18,12 +19,13 @@ return (NULL);
 i = 0;
 while (str[i] != '\0')
 i++;
-sru = malloc(sizeof(char) * (i + 1));
+aaa = malloc(sizeof(char) * (i + 1));
+if (aaa == NULL)
+return (NULL);
 
-if (sru == NULL)
 for (r = 0; str[r]; r++)
-sru[r] = str[r];
+aaa[r] = str[r];
 
-return (sru);
+return (aaa);
 }
 
