92b081694bbe73c52f38463732b9790cf4d70074
Argen-tal <carlpeacebringer@gmail.com>
2023-05-23 15:16:48 -0700

    Hours of the day

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,12 @@
  */
 int print_last_digit(int n)
 {
-	int lastdigit = n % 10;
-
+	int lastdigit;
+	if (n < 0)
+		n = -n;
+	lastdigit = n % 10;
+	if (lastdigit < 0)
+		lastdigit = -lastdigit;
+	_putchar (lastdigit + '0');
 	 return (lastdigit);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints the hours and Minutes of the day
+ *
+ *
+ */
+void jack_bauer(void)
+{
+	int hrs;
+	int mins;
+
+	for (hrs = 0; hrs <= 23; hrs++)
+	{
+		for (mins = 0; mins <= 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
