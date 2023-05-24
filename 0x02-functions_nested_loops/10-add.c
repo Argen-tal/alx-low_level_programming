fc9d94e2cac5b01739ef955ab5cc7596a7eb373b
Argen-tal <carlpeacebringer@gmail.com>
2023-05-23 21:22:09 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -> adds two integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,20 +1,35 @@
 #include "main.h"
 /**
- * times_table - Multiplication tables from 0 - 9
- * 
- * Return: null
+ * times_table - time table 0 - 9
  */
 void times_table(void)
 {
-	while (i <= 0)
+	int l;
+	int m;
+	int n;
+
+	for (l = 0; l < 10; l++)
 	{
-		while (j <= 9)
-    		{
-			int product = i * j;
-        		_putchar(product);
-        		j++;
-    		}
-    			_putchar('\n');
-			i++;
+		for (m = 0; m < 10; m++)
+		{
+			n = l * m;
+			if (m == 0)
+				_putchar(n + '0');
+			if (m != 0 && n < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(n + '0');
+			}
+			else if (n >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_puchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
 }
