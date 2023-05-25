8a86f1532363a1d538b9d8c27d925040d3c71fd1
Argen-tal <carlpeacebringer@gmail.com>
2023-05-25 07:36:27 -0700

    Test

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
