ec779a4715718dd1d63f4533bf950fcac1dbad33
Argen-tal <carlpeacebringer@gmail.com>
2023-05-25 19:42:16 -0700

    Triangle bug fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,21 +7,21 @@
  */
 void print_triangle(int size)
 {
-int count = 0;
-int n;
+	int row;
+	int col;
 
-if (size <= 0)
-_putchar('\n');
-return;
-while (count < size)
-{
-n = 0;
-while (n <= count)
-{
-_putchar('#');
-n++;
-}
-_putchar('\n');
-count++;
-}
+	if (size <= 0)
+	{
+		_putchar ('\n');
+		return;
+	}
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size - row -1; col++)
+			_putchar(' ');
+		for (col = 0; col <= row; col++)
+			_putchar('#');
+	_putchar('\n');
+	}
+
 }
