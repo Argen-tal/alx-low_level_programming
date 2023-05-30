b80c5f42499c9b83065b08c41ac71490a1fd1044
Argen-tal <carlpeacebringer@gmail.com>
2023-05-30 12:53:03 -0700

    Quick fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,14 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
 	if (a == NULL || n <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i != n - 1)
