cc66537e3b2b38e0f01082b90eed81861bde8279
Argen-tal <carlpeacebringer@gmail.com>
2023-06-05 18:06:32 -0700

    Function to copy memory area added

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 /* HEADER FILES GOES HERE */
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 
 #endif /*MAIN_H */
