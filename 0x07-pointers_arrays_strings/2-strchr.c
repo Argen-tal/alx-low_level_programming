c5d0c80e9386864132b71bc42ea1716b51680bd5
Argen-tal <carlpeacebringer@gmail.com>
2023-06-05 18:28:28 -0700

    string search function added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be checked.
+ * @c: charater search.
+ *
+ * Return: Pointer to c if the character is found. NULL otherwise.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+
+			return (s);
+		s++;
+	}
+	if (*s == c)
+		return (s);
+
+	return (NULL);
+}	
