94e249a14b74477808333c87ae257370e071b94d
Argen-tal <carlpeacebringer@gmail.com>
2023-06-07 19:53:02 -0700

    function fo calculate factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: given integer.
+ *
+ * Return: -1 if n is less than 0. 1 if n is equivalent to zero
+ * factorial of n otherwise.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
+
