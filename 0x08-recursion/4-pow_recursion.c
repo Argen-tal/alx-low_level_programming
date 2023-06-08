c37e6f16a744a8450ef3b590a779384daf084b79
Argen-tal <carlpeacebringer@gmail.com>
2023-06-08 05:52:41 -0700

    Trailing white spaces in pow function removed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * _pow_recursion - Calculates the value of x raised to the power of y.
- * @x: The base number.
- * @y: The exponent.
+ * _pow_recursion - function that returns the value of x raised
+ * to the power of y.
+ * @x: integer value
+ * @y: integer value
  *
- * Return: The result of x raised to the power of y.
- *         -1 if y is negative.
+ * Return: -1 if y is less than 0. 1 if y is 0. Otherwuise X^Y
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return -1;
+		return (-1);
 	else if (y == 0)
-		return 1;
+		return (1);
 	else
-		return x * _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 }
-
