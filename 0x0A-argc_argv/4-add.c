51caf3bed4f02fabd8fe63a5bf6f352d689aaf97
Argen-tal <carlpeacebringer@gmail.com>
2023-06-09 19:17:09 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Sums up valid numbers from command-line arguments
+ * @argc: Count of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    int sum = 0;
+    int i;
+
+    for (i = 1; i < argc; i++)
+    {
+        int num = atoi(argv[i]);
+        if (num == 0 && *argv[i] != '0')
+        {
+            printf("Error\n");
+            return (1);
+        }
+        sum += num;
+    }
+
+    printf("%d\n", sum);
+    return (0);
+}
+
