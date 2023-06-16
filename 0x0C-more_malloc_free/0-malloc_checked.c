8231dca2dbef36879f22cf81391d899982a27c63
Argen-tal <carlpeacebringer@gmail.com>
2023-06-15 21:48:42 -0700

    fixed trailing white spaces in c13, 16, 19

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,18 +3,18 @@
 #include <stdio.h>
 /**
  * malloc_checked - allocates memory using malloc
- * @b:number of data type.
+ * @b: number of data type.
  *
- * Return - pointee to allocated memory.
+ * Return: 98 for failed. pointer to allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr_malloc;
-	
+
 	ptr_malloc = malloc(b);
+
 	if (ptr_malloc == NULL)
-		exit (98);
+		exit(98);
 
 	return (ptr_malloc);
-	
 }
