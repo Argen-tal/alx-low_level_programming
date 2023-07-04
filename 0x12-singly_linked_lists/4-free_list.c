dfdd6f1f1137da1743e8a4f2e570f4de4f5ab89d
Argen-tal <carlpeacebringer@gmail.com>
2023-07-04 15:51:42 -0700

    fixed bug

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,16 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current;
 	list_t *temp;
 
 	while (head)
 	{
-		current = head;
 		temp = head->next;
 		free(head->str);
 		free(head);
 		head = temp;
 	}
 }
-
