14ea98b163b0845339d614750f8ac0b1aa35393b
Argen-tal <carlpeacebringer@gmail.com>
2023-09-28 17:54:35 -0700

    added more functions

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
 #include "main.h"
+#include <stddef.h>
 
 /**
- * binary_to_uint - Converts a binary string to an unsigned integer.
- * @b: The binary string to convert.
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a pointer to a string of 0 and 1
  *
- * Return: The converted unsigned integer or 0 if an invalid character is encountered.
+ * Return: the converted number, or 0 if there is one or more chars
  */
 unsigned int binary_to_uint(const char *b)
 {
-if (b == NULL)
-return (0);
+	unsigned int num = 0, mult = 1;
+	int len;
 
-unsigned int result = 0;
+	if (b == NULL)
+		return (0);
 
-for (int i = 0; b[i] != '\0'; i++) {
-if (b[i] == '0')
-{
-result <<= 1;  /* Shift left by 1 (multiply by 2) */
-}
-else if (b[i] == '1') {
-result <<= 1;  /* Shift left by 1 */
-result |= 1;   /* Set the least significant bit to 1 */
-}else
-{
-/* Invalid character encountered (not '0' or '1') */	    
-return (0);
-}
-}
-return (result);
-}
+	for (len = 0; b[len];)
+		len++;
 
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
+}
